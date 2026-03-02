17d7feb51f7802772402c71b786248f843527e4d
MiguelPinheiro2409 <135389129+MiguelPinheiro2409@users.noreply.github.com>
2026-03-01 23:47:48 -0300

    Add files via upload

---
+++ experimento_busca/target/release/deps/experimento_busca.d
@@ -0,0 +1,5 @@
+C:\Users\Miguel\.cargo\experimento_busca\target\release\deps\experimento_busca.d: src\main.rs
+
+C:\Users\Miguel\.cargo\experimento_busca\target\release\deps\experimento_busca.exe: src\main.rs
+
+src\main.rs:
---
+++ experimento_busca/target/release/experimento_busca.d
@@ -0,0 +1,1 @@
+C:\Users\Miguel\.cargo\experimento_busca\target\release\experimento_busca.exe: C:\Users\Miguel\.cargo\experimento_busca\src\main.rs
